f494ca761147dba2cc07b89bab92d1c6280eb3c6
Aniket Suryawanshi <twomindscreate17@gmail.com>
2024-07-16 20:29:21 +0530

    Adding PUT Method
    Adding PUT Method

--- app/serializers.py
+++ app/serializers.py
@@ -13,40 +13,40 @@ class TeamSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'description', 'created_at']
 
 class MembershipSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    team = TeamSerializer()
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    team = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all())
 
     class Meta:
         model = Membership
         fields = ['id', 'user', 'team', 'date_joined', 'is_admin']
 
 class TaskSerializer(serializers.ModelSerializer):
-    assigned_to = UserSerializer()
-    team = TeamSerializer()
+    assigned_to = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    team = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all())
 
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'assigned_to', 'team', 'status', 'created_at', 'due_date', 'updated_at', 'original_task']
 
 class FeedbackSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    task = TaskSerializer()
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    task = serializers.PrimaryKeyRelatedField(queryset=Task.objects.all())
 
     class Meta:
         model = Feedback
         fields = ['id', 'task', 'user', 'comment', 'created_at']
 
 class PermissionSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    task = TaskSerializer()
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    task = serializers.PrimaryKeyRelatedField(queryset=Task.objects.all())
 
     class Meta:
         model = Permission
         fields = ['id', 'user', 'task', 'can_create', 'can_read', 'can_update', 'can_delete']
 
 class ReminderSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    task = TaskSerializer()
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    task = serializers.PrimaryKeyRelatedField(queryset=Task.objects.all())
 
     class Meta:
         model = Reminder
